689437f54811887218c3f56c65998102c4d64447
Gopi Selvamani <gopiselvamani@Gopis-MacBook-Air.local>
2025-02-14 17:17:53 +0400

    main

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+//components
+import Main from './components/Total'
+
+function App() {
+  return (
+
+    <div className="App">
+      REACT MOVIE SEARCH
+      <Main />
+    </div>
+    
+  );
+}
+
+export default App;
